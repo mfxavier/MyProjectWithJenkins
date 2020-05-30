be6068dd271f3f6adcac4c0dddcd982191523a89
Marlon de França Xavier <marlon.xavier@icloud.com>
2020-05-30 19:56:03 -0300

    Classe de Teste

---
+++ TesteJenkins.java
@@ -0,0 +1,11 @@
+package myClassJava;
+
+public class TesteJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste de versionamento com Jenkins");
+
+	}
+
+}
